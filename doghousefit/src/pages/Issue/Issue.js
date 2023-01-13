b2f3e39ca1b309e136129b08480cb6dd0da6bb15
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 00:10:03 -0500

    More formatting and improvements

--- doghousefit/src/pages/Issue/Issue.js
+++ doghousefit/src/pages/Issue/Issue.js
@@ -1,39 +1,60 @@
 import Navbar from "../../components/Navbar";
 import { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 const Issue = () => {
     const { id } = useParams();
-    // const [res.data, setres.data] = useState()
-    const [issue, setIssue] = useState()
-    const [title, setTitle] = useState()
-    const [date, setDate] = useState()
-    const [tags, setTags] = useState()
+    const navigate = useNavigate();
+    const [issueData, setIssueData] = useState({
+        issue: undefined,
+        title: undefined,
+        date: undefined,
+        tags: undefined
+    });
     
     useEffect(() => {
         axios.get(`http://localhost:3001/newsletters/${id}`)
-            .then(
-                res => {
-                    console.log(res.data[0])
-                    setIssue(res.data[0].issue)
-                    setTitle(res.data[0].title)
-                    setDate(res.data[0].date)
-                    setTags(res.data[0].tags)
-                }
-            )
-            .catch(error => {
-                console.log(error)
-              })
-        
-    }, [])
+            .then(res => {
+                console.log(res.data[0])
 
-    return (
-        <>
-            <Navbar />
-            <h1>Issue #: {issue}</h1>
-        </>
-    )
+                // Redirects if there is no newsletter
+                if(res.data[0] === undefined) {
+                    navigate('/newsletters')
+                }
+                
+                setIssueData({
+                    issue: res.data[0].issue,
+                    title: res.data[0].title,
+                    date: res.data[0].date,
+                    tags: res.data[0].tags
+                });
+            }).catch(error => {
+                console.log(error)
+            })
+        }, [])
+        
+        const formatDate = (dateString) => {
+            const date = new Date(dateString);
+            const monthNames = ["January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December"
+            ];
+            const day = date.getDate();
+            const monthIndex = date.getMonth();
+            const year = date.getFullYear();
+          
+            return monthNames[monthIndex] + ' ' + day + ', ' + year;
+          }
+
+        return (
+            <>
+                <Navbar />
+                <h1>Issue #: {issueData.issue}</h1>
+                <h1>Title: {issueData.title}</h1>
+                <h1>Date: {formatDate(issueData.date)}</h1>
+                <h1>Tags: {issueData.tags?.map((tag) => <li key={tag}>{tag}</li>)} </h1>
+            </>
+        )
 }
 
 export default Issue;
