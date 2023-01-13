30e48958d54895eec1b109327a0af5e7f69973f5
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 16:20:53 -0500

    please work

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/newsletters', require('./routes/newsletterRoutes'))
+app.use('/api/newsletters', require('./routes/newsletterRoutes'))
 
 app.use(errorHandler)
 
