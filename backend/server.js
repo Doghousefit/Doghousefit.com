6eee3223eea9f99d6caefc4c5284b0120a8d8060
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 22:04:08 -0500

    Im going to make it into 2 repos

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// app.use('/api/newsletters', require('./routes/newsletterRoutes'))
-app.use('/newsletters', require(path.join(__dirname, 'routes', 'newsletterRoutes.js')));
-
+app.use('/api/newsletters', require('./routes/newsletterRoutes'))
+// app.use('/newsletters', require(path.join(__dirname, 'routes', 'newsletterRoutes.js')));
 
 app.use(errorHandler)
 
@@ -30,9 +29,11 @@ app.listen(port, () => {
     // console.log(process.env)
 })
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend', 'build')));
-    app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+//     app.get('/*', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+//     })
+// }
+
+module.exports = app
