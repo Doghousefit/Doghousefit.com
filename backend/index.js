ee80964f8e71162b85976bde555b5db83bcb181b
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 22:24:51 -0500

    renamed to index

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config({path: __dirname+'/.env'});
-  }
-//   const dotenv = require('dotenv').config()
+}
 
 const express = require('express')
 const connectDB = require('./config/db')
@@ -20,20 +19,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('//newsletters', require('./routes/newsletterRoutes'))
-// app.use('/newsletters', require(path.join(__dirname, 'routes', 'newsletterRoutes.js')));
 
 app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`)
-    // console.log(process.env)
 })
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../frontend', 'build')));
-//     app.get('/*', (req, res) => {
-//       res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
-//     })
-// }
 
 module.exports = app
