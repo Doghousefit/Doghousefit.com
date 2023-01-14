240ffc6c2d553b639e5c07f0284c1c4101331999
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 22:20:54 -0500

    updates

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/newsletters', require('./routes/newsletterRoutes'))
+app.use('//newsletters', require('./routes/newsletterRoutes'))
 // app.use('/newsletters', require(path.join(__dirname, 'routes', 'newsletterRoutes.js')));
 
 app.use(errorHandler)
