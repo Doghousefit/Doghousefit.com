23f4e1bfc606abb37fd278ffb67cd8aba13f6efb
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 23:49:16 -0500

    port

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,12 @@ app.use('/newsletters', require('./routes/newsletterRoutes'))
 //   console.log("Running on port 5000.");
 // });
 
+const port = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Mongodb connected");
-    app.listen(5000, () => {
-      console.log(`Server is listening on port ${port}`);
+    // console.log(process.env);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is listening on port: ${port}`);
     });
   }).catch((err) => {
     console.log({ err });
