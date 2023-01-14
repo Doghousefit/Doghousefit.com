6f4434ca13e4c931a6533d994ee46bcff64dfe4f
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 22:46:34 -0500

    a

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,32 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({path: __dirname+'/.env'});
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config({path: __dirname+'/.env'});
+// }
 
 const express = require('express')
-const connectDB = require('./config/db')
+// const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const cors = require('cors');
-const path = require('path');
-
+// const path = require('path');
+const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
 const port = 3001
 
 const app = express()
+
+
+
+const connectDB = async () => {
+    try {
+        console.log(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        mongoose.set('strictQuery', false);
+        
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    } catch(error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
 
 connectDB()
 
