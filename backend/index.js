1ad6bf3a8619419428134859fedb65f1362b3585
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 22:56:47 -0500

    b

--- backend/index.js
+++ backend/index.js
@@ -1,57 +1,17 @@
-// if (process.env.NODE_ENV !== 'production') {
-//     require('dotenv').config({path: __dirname+'/.env'});
-// }
+const express = require("express");
 
-const express = require('express')
-// const connectDB = require('./config/db')
-const { errorHandler } = require('./middleware/errorMiddleware')
-const cors = require('cors');
-// const path = require('path');
-const dotenv = require('dotenv').config()
-const mongoose = require('mongoose')
-import newsletterRoutes from './routes/newsletterRoutes';
-const port = 3001
+const app = express();
 
-const app = express()
-
-// const connectDB = async () => {
-//     try {
-//         console.log(process.env.MONGO_URI)
-//         const conn = await mongoose.connect(process.env.MONGO_URI)
-//         mongoose.set('strictQuery', false);
-        
-//         console.log(`MongoDB connected: ${conn.connection.host}`)
-//     } catch(error) {
-//         console.log(error)
-//         process.exit(1)
-//     }
-// }
-
-// connectDB()
-
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-app.get('/', (req, res) => {
-    res.json({message: 'connected to api'})
-})
-// app.use('/newsletters', require('./routes/newsletterRoutes'))
-app.use('/newsletters', newsletterRoutes)
-
-app.use(errorHandler)
-
-// app.listen(port, () => {
-//     console.log(`Server started on port: ${port}`)
-// })
-
-// module.exports = app
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("Mongodb connected");
-    server.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  }).catch((err) => {
-    console.log({ err });
-    process.exit(1);
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
+app.get("/api", (req, res) => {
+    res.send("API on Vercel");
+  });
+
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
