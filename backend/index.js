3ae097782d826a899969b04e83e994d3b6c90935
Rodz Amor <76978597+RodzAmor@users.noreply.github.com>
2023-01-13 22:31:05 -0500

    test

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.get('/', (req, res) => {
+    res.json({message: 'connected to api'})
+})
 app.use('/newsletters', require('./routes/newsletterRoutes'))
 
 app.use(errorHandler)
